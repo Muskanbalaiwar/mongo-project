f0db1fb87c4569ebd2f1b55d34daa33ee931b270
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-06-09 12:21:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const mongoose = require('mongoose');
+// const mongoConnect = require('./util/database').mongoConnect;
+// const User = require('./models/user');
 
 const app = express();
 
@@ -13,25 +14,30 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('648192a48448b970266bcd97')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('648192a48448b970266bcd97')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://MuskanGupta:EyQ9RKz99FTwUO4X@cluster0.xuil7ce.mongodb.net/shop?retryWrites=true&w=majority')
+.then(res=>{
   app.listen(3000);
-});
+})
+.catch(err=>{
+  console.log(err);
+})
+
